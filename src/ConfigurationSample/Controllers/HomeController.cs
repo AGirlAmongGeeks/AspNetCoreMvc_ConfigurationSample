b331cb38d5955216335dae418f06dc93eb89680d
Karina <karina.skorka@gmail.com>
2017-02-17 02:24:17 +0100

    Init commit.

---
+++ src/ConfigurationSample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using ConfigurationSample.Helpers;
+using ConfigurationSample.Model;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationSample.Controllers
+{
+    public class HomeController : Controller
+    {
+		private EmailMessages EmailMessages { get; set; }
+
+		public HomeController(IOptions<EmailMessages> config)
+		{
+			EmailMessages = config.Value;
+		}
+
+		public IActionResult Index()
+        {
+			#region Retrieve configuration (connection string) in traditional way.
+			var configuration = ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
+
+			//3 ways to get the same configuration.
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+			var connectionString2 = configuration.GetSection("ConnectionStrings")["DefaultConnection"];
+			var connectionString3 = configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+			#endregion
+
+			#region Strongly typed configuration.
+			var firstMessage = EmailMessages.FirstMessage;
+
+			#endregion
+
+			return View();
+        }
+    }
+}
---
+++ src/ConfigurationSample/Model/EmailMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationSample.Model
+{
+	//In case you wonder - yes, in 'normal' project, these class should be stored in separeted files :).
+	public class EmailMessages
+	{
+		public bool SendingEnabled { get; set; }
+		public FirstMessage FirstMessage { get; set; }
+		public SecondMessage SecondMessage { get; set; }
+	}
+
+	public class FirstMessage
+	{
+		public string Title { get; set; }
+		public string Message { get; set; }
+	}
+
+	public class SecondMessage
+	{
+		public string Message { get; set; }
+		public string Title { get; set; }
+	}
+}
